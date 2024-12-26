5129802f729de41395f8d8cf7740b0d9f5030568
Ken Kousen <ken.kousen@kousenit.com>
2024-12-26 15:29:34 -0500

    Add markdown library

--- gemini_demo.py
+++ gemini_demo.py
@@ -1,5 +1,6 @@
 import google.generativeai as genai
 import os
+import markdown
 
 GOOGLE_API_KEY = os.environ['GOOGLEAI_API_KEY']
 
@@ -9,13 +10,20 @@ for m in genai.list_models():
     if 'generateContent' in m.supported_generation_methods:
         print(m.name)
 
+client = genai.GenerativeModel('models/gemini-2.0-flash-thinking-exp')
+
+response = client.generate_content(
+    contents='Explain Reinforcement Learning in simple terms.'
+)
+
+print(markdown.markdown(response.candidates[0].content.parts[0].text))
+
 model_info = genai.get_model('models/gemini-2.0-flash-exp')
 print(model_info.input_token_limit, model_info.output_token_limit)
 
 model = genai.GenerativeModel('models/gemini-2.0-flash-exp')
-prompt = "The quick brown fox jumps over the lazy dog."
+prompt = "What are five features of Python of interest to java developers?"
 
 response = model.generate_content(prompt)
 print(response.text)
 print(response.usage_metadata)
-
